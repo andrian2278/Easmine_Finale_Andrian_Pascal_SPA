d321fd5536b64994d9a5ca216f10f1060c16a8f7
Andrian Pascal <andrian.pascal@stud.tecnicosuperiorekennedy.it>
2021-07-19 15:17:35 +0200

    1.9(Commit Finale)

--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -12,20 +12,13 @@ import { OFFERTS_TURING } from '../models/DATA_Api';
 export class PageListComponent implements OnInit {
    offerts:OFFERTS_TURING[]=[];
   constructor(private route: Router, private _Db: ApiRestService) { }
-  indicator=0;
   
-status1=false;
 
   ngOnInit(): void {
     this._Db.getPark().subscribe(data => {
       this.offerts = data;
       
-      this.offerts.forEach( element=> {
-        if (element.people_entered !> element.people_out ) {
-         this.status1 = true;
-        }
-       console.log(this.status1)
-      });
+      
   })
 }
 }
